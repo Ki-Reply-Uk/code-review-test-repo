118bda16f177fc9a0fc9bf39dd39768296444bd6
KiUKCodeFixAgent <152207718+KiUKCodeFixAgent@users.noreply.github.com>
2024-07-12 12:40:11 +0100

    Refactor code to use `sys.stdout.write` instead of `print` for better output control in letters.py.

--- letters.py
+++ letters.py
@@ -96,7 +96,8 @@ def play_once():
 
         except (EOFError, KeyboardInterrupt):
             # End-Of-File : the user
-            print('\nOK; give up if you like.')
+            import sys
+            sys.stdout.write('\nOK; give up if you like.\n')
             return
 
         if len(query)==1:
