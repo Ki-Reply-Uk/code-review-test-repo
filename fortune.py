d1104892cf3547e2391c2ca36c44823cf9f9f593
KiUKCodeFixAgent <152207718+KiUKCodeFixAgent@users.noreply.github.com>
2024-07-15 16:36:35 +0100

    Add Hello World print statement at the start of fortune.py

--- fortune.py
+++ fortune.py
@@ -1,4 +1,7 @@
 
+# Print a hello world message
+print("Hello World")
+
 # fortune.py -- chooses a random fortune, as the fortune(8) program in
 #               the BSD-games package does
 #
@@ -21,7 +24,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-
 
 import struct, random, string
 
