00ddf54eab677d511e946950eea36f985f82d2b2
KiUKCodeFixAgent <152207718+KiUKCodeFixAgent@users.noreply.github.com>
2024-07-22 09:53:24 +0100

    Replace print statement with logging function in `strfile.py`

--- strfile.py
+++ strfile.py
@@ -94,7 +94,5 @@ write_long(0)                           # Flags; we'll set them to zero
 output.write(delimiter + '\0'*(LONG_SIZE-1))
 output.close()
 
-print('''"%s.dat" created
-There were %i strings
-Longest string: %i bytes
-Shortest string: %i bytes''' % (filename, numstr, longest, shortest))
+import logging
+logging.info('"%s.dat" created\nThere were %i strings\nLongest string: %i bytes\nShortest string: %i bytes', filename, numstr, longest, shortest)
