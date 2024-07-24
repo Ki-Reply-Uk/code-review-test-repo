dce971806bff193f4cbc881cc993c1a1106c9f63
KiUKCodeFixAgent <152207718+KiUKCodeFixAgent@users.noreply.github.com>
2024-07-24 14:20:19 +0100

    Replace print statement with built-in function in specified range in letters.py

--- letters.py
+++ letters.py
@@ -96,7 +96,7 @@ def play_once():
 
         except (EOFError, KeyboardInterrupt):
             # End-Of-File : the user
-            print '\nOK; give up if you like.'
+            built-in function('\nOK; give up if you like.')
             return
 
         if len(query)==1:
