ab7db464a22a10ae33c5a2925cf75293a18f515d
KiUKCodeFixAgent <152207718+KiUKCodeFixAgent@users.noreply.github.com>
2024-07-31 15:12:57 +0100

    Revert first change in `fortune.py`

--- fortune.py
+++ fortune.py
@@ -51,7 +51,7 @@ def get(filename):
     datfile = open(filename+'.dat', 'r')
     data = datfile.read(5 * LONG_SIZE)
     if is_64_bit:
-        _, n1, n2, _, _, s1, s2, f1, f2 = struct.unpack('!9L', data)
+        _, n1, n2, l1, l2, s1, s2, f1, f2 = struct.unpack('!9L', data)
         numstr   = n1 + (n2 << 32)
         shortlen = s1 + (s2 << 32)
         flags    = f1 + (f2 << 32)
