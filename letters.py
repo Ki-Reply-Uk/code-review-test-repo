adf5dc1c1ac0f73cefec8f6da992255152e16103
KiUKCodeFixAgent <152207718+KiUKCodeFixAgent@users.noreply.github.com>
2024-08-13 11:54:10 +0100

    Replace print with sys.stdout.write in letters.py to address SonarQube issue

--- letters.py
+++ letters.py
@@ -96,7 +96,8 @@ def play_once():
 
         except (EOFError, KeyboardInterrupt):
             # End-Of-File : the user
-            print('\nOK; give up if you like.')
+            import sys
+            sys.stdout.write('\nOK; give up if you like.\n')
             return
 
         handle_query(query, possibilities, asked, letter_stats, choice)
