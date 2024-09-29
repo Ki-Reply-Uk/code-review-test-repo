4d64e2355e037daa838f40c358fb0d013d47e1d7
JamesWilson626 <jam.wilson@reply.com>
2024-09-29 15:16:33 +0100

    Remove unused local variable "flags" from fortune.py to address SonarQube findings.

--- fortune.py
+++ fortune.py
@@ -51,11 +51,10 @@ def get(filename):
     datfile = open(filename+'.dat', 'r')
     data = datfile.read(5 * LONG_SIZE)
     if is_64_bit:
-        _, _, n1, n2, l1, l2, s1, s2, f1, f2 = struct.unpack('!10L', data)
+        _, n1, n2, l1, l2, s1, s2, f1, f2 = struct.unpack('!9L', data)
         numstr   = n1 + (n2 << 32)
         longlen  = l1 + (l2 << 32)
         shortlen = s1 + (s2 << 32)
-        flags    = f1 + (f2 << 32)
     else:
         _, numstr, longlen, shortlen, _ = struct.unpack('5l', data)
 
