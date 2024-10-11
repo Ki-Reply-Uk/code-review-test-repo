8ff168e9946c6849a4799aac62584244cc9c08d6
JamesWilson626 <jam.wilson@reply.com>
2024-10-11 16:40:21 +0100

    Remove unused local variable 'version' in fortune.py to resolve SonarQube issue

--- fortune.py
+++ fortune.py
@@ -51,7 +51,7 @@ def get(filename):
     datfile = open(filename+'.dat', 'r')
     data = datfile.read(5 * LONG_SIZE)
     if is_64_bit:
-        _, _, n1, n2, l1, l2, s1, s2, f1, f2 = struct.unpack('!10L', data)
+        _, n1, n2, l1, l2, s1, s2, f1, f2 = struct.unpack('!9L', data)
         numstr   = n1 + (n2 << 32)
         longlen  = l1 + (l2 << 32)
         shortlen = s1 + (s2 << 32)
